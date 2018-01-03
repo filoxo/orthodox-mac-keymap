fca22fdd803f03f803535a3de8b8ff9c593bd204
Carlos Filoteo <cfiloteo@ancestry.com>
2018-01-03 11:55:55 -0700

    Adjust some spacing

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -73,21 +73,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = KEYMAP( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                                KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-  _______, _______, _______, _______, _______, _______, KC_LCTL, KC_LALT, _______,          _______, KC_RALT, KC_RCTL, _______, KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______,          _______, _______, _______, KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_F11,  KC_F12 \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                                              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+  _______, _______, _______, _______, _______, _______, KC_LCTL, KC_LALT, _______,         _______, KC_RALT, KC_RCTL,_______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______,         _______, _______, _______,KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 \
 ),
 
 [_RAISE] = KEYMAP( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  _______, _______, _______, KC_HOME, KC_END,  _______, XXXXXXX, _______,          _______, XXXXXXX, _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_DEL,  _______, _______, _______, KC_HOME, KC_END,  _______, XXXXXXX, _______,         _______, XXXXXXX, _______, KC_F6,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______,_______, _______, _______, _______, _______ \
 ),
 
 [_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______,                                                                _______, _______, _______, _______, _______, KC_DEL,  \
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, _______, XXXXXXX, _______,          _______, XXXXXXX, _______, AG_SWAP, QWERTY , COLEMAK, DVORAK,  _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  _______, RESET,   _______, _______, _______, _______,                                                               _______, _______, _______, _______, _______, KC_DEL,  \
+  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, _______, XXXXXXX, _______,         _______, XXXXXXX, _______, AG_SWAP, QWERTY , COLEMAK, DVORAK,  _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 )
 
 
