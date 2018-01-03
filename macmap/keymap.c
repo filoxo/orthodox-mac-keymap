a18e2ae5a3fff3fe70fe7eb6c500bbea0460effd
Carlos Filoteo <cfiloteo@ancestry.com>
2018-01-03 11:57:39 -0700

    Remove LS__SPC modtap since Autoshift

--- macmap/keymap.c
+++ macmap/keymap.c
@@ -50,14 +50,12 @@ enum custom_keycodes {
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-#define LS__SPC MT(MOD_LSFT, KC_SPC)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = KEYMAP( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LEFT, XXXXXXX, KC_DOWN,         KC_UP,  XXXXXXX, KC_RIGHT,KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LCTL, KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    LOWER,   KC_BSPC, KC_LGUI,         KC_ENT, LS__SPC, RAISE,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH \
+  KC_LCTL, KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    LOWER,   KC_BSPC, KC_LGUI,         KC_ENT, KC_SPC,  RAISE,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH \
 ),
 
 [_COLEMAK] = KEYMAP(\
